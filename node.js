d4b53e672c7193b64b4a176c389a66a4bd291bf7
mahmood1994s <63569255+mahmood1994s@users.noreply.github.com>
2021-03-25 02:19:03 +0000

    Add files via upload

--- node.js
+++ node.js
@@ -1,16 +1,19 @@
-inspirationalMsg = ['Act as if what you do makes a difference. It does.',
+
+let inspirationalMsg = ['Act as if what you do makes a difference. It does.',
         'Success is not final, failure is not fatal: it is the courage to continue that counts.',
         'Never bend your head. Always hold it high. Look the world straight in the eye.',
         'What you get by achieving your goals is not as important as what you become by achieving your goals.',
         'Believe you can and you\'re halfway there.',
         'It isn\'t where you came from. It\'s where you\'re going that counts.',
-        'When you have a dream, you\'ve got to grab it and never let go.','People are trapped in history and history is trapped in them.',
+        'When you have a dream, you\'ve got to grab it and never let go.'];
+ let history = ['People are trapped in history and history is trapped in them.',
         'History is who we are and why we are the way we are. ',
         'Revolutions are the locomotives of history.',
         'Blood alone moves the wheels of history.',
         'I do not speak Hebrew, but I understand that it has no word for ‘history’. The closest word for it is memory. ',
         'Fear not for the future, weep not for the past. ',
-        'History is a relay of revolutions.','How many ears does Spock have? Three. The left ear, the right ear, and the final front-ear!',
+        'History is a relay of revolutions.'];
+let facts = ['How many ears does Spock have? Three. The left ear, the right ear, and the final front-ear!',
         'What do you call someone with no body and no nose? Nobody knows!',
         'What do you call a fish with no eye? Fsh!',
         'I went to buy some camouflage trousers the other day but I couldn’t find any.',
@@ -18,8 +21,10 @@ inspirationalMsg = ['Act as if what you do makes a difference. It does.',
         'The rotation of earth really makes my day.',
         'I once had a teacher with a lazy eye. She couldn’t control her pupils.'];
 const messge = () => {
-let x = Math.floor(Math.random() *21)
-console.log(inspirationalMsg[x]);
+const w = [inspirationalMsg , history , facts] ;   
+let x = Math.floor(Math.random() * 3)
+let y = Math.floor(Math.random() * 7)
+console.log(w[x][y]);
 
 };
 messge();
